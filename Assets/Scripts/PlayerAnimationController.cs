ead05b65b6a8e13cf5a821da763a3e40a580519a
DrAP <agus10pg@gmail.com>
2018-05-12 15:07:29 +0200

    Add terrain, character and standard assets

---
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimationController : MonoBehaviour {
+    
+    Animator animator;
+    int jumpHash = Animator.StringToHash("Jump");
+    int runHash = Animator.StringToHash("Running");
+
+	// Use this for initialization
+	void Start ()
+    {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        float jump = Input.GetAxis("Jump");
+
+        animator.SetBool(runHash, (horizontal != 0 || vertical != 0));
+        if (jump==1)
+            animator.SetTrigger(jumpHash);
+    }
+}
